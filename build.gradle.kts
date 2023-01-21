28ee093d82f9ff8ed5b5bbf7a551bf4e0763f258
duharev <Alisa13062405>
2023-01-21 20:15:36 +0300

    m1l1 first app and test

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+
+plugins {
+    kotlin("jvm") apply false
+}
+
+group = "ru.duharev.reitings"
+version = "1.0-SNAPSHOT"
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+
+}
+subprojects {
+    group = rootProject.group
+    version = rootProject.group
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "11"
+    }
+}
+
---
+++ m1l1-quickstart/src/test/kotlin/FirstTest.kt
@@ -0,0 +1,12 @@
+package ru.duharev.reitings.m1l1
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class FirstTest {
+
+    @Test
+    fun firstTest() {
+        assertEquals(3, 1 + 2)
+    }
+}
